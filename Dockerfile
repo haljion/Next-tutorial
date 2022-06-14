827b89565f6ed625f2f1d8840b7db91eed8385b7
haljion <haljion.helmold@gmail.com>
2022-06-15 02:59:52 +0900

    tutorial complete

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,11 @@ FROM node:18.3.0
 WORKDIR /app
 
 # npm install -g yarn 入ってる？
+
+# next.jsアプリ作成
+# npx create-next-app nextjs-blog --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
+# 
+
+# サーバー立ち上げ
+# cd nextjs-blog
+# npm run dev
--- app/nextjs-blog/pages/posts/[id].js
+++ app/nextjs-blog/pages/posts/[id].js
@@ -1,14 +1,23 @@
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Layout from "../../components/layout";
 
+import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
+
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
@@ -22,7 +31,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
+
+  // Catch-all Routes url連結
+  // const postData = await getPostData(params.id.join("/"));
+
   return {
     props: {
       postData,
